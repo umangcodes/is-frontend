64519fc8a74066182f83f5ab55e0a0d1284f3443
umangcodes <umanga.eng@gmail.com>
2023-12-05 11:21:15 -0500

    chore: link update

--- components/navigation/Copyright.js
+++ components/navigation/Copyright.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Typography } from '@mui/material';
 import Link from 'next/link';
+import links from '@/utils/links';
 
 function Copyright(props) {
     return (
       <Typography variant="body2" color="text.secondary" align="center" {...props}>
         {'Copyright © '}
-        <Link color="inherit" href="https://mui.com/">
+        <Link color="inherit" href={links.home}>
           boilerplate
         </Link>{' '}
-        {new Date().getFullYear()}
+        {new Date().getFullYear() + ` - ${(new Date().getFullYear() + 1).toString().slice(2,4)}`}
         {'.'}
       </Typography>
     );
