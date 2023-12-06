5134ec2a0ef0ac5b873d4db1ae8d418c42f39978
umangcodes <umanga.eng@gmail.com>
2023-12-06 10:03:44 -0500

    chore: landing page update and how it works update

---
+++ app/howitworks/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '../globals.css'
+import styles from "@/app/global.module.css"
+// import {Footer, Header} from "@/components/index"
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Instant Shine | Car detailing',
+  description: 'Car care | car detailing | auto interior detailing',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className + ` ${styles.page}`}>
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ components/cards/HowItWorks.js
@@ -0,0 +1,7 @@
+function HowItWorks() {
+  return (
+    <div></div>
+  )
+}
+
+export default HowItWorks
